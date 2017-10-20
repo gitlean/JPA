a67c0c48c4eceb96888b5fe9d8e0007d56cba009
jerry <jerry@example.com>
2017-10-20 21:45:18 +0800

    commit

--- testGit/src/com/isoft/TestMain.java
+++ testGit/src/com/isoft/TestMain.java
@@ -1,10 +1,11 @@
-package app;
+package com.isoft;
 
 public class TestMain {
 
 	//这是一个注释
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("你好");
 
 	}
 
